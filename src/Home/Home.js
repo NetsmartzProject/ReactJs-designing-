70fd27cbfa900365badf90cf1a69be191aaf50ce
Abhinandan <royabhinandan.002@gmail.com>
2024-04-24 12:15:08 +0530

    This Project Consist of Designing

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <>
+   <div id = "root-main">
+    <div className='flag'>
+     <p></p>
+    </div>
+   </div>
+   </>
+  )
+}
+
+export default Home
